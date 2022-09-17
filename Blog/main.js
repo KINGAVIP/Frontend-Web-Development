b0b858660fcc731716ef4749d250692f562b93ca
Avi Pruthi <avipruthi8@gmail.com>
2022-09-17 19:04:43 +0530

    MEGA PROJECT-blog website complete Frontend

--- Blog/main.js
+++ Blog/main.js
@@ -13,3 +13,33 @@ closebtn.addEventListener("click",()=>{
     closebtn.style.display='none';
 });
 
+
+
+const rightbtn=document.querySelector(".show-aside-button");
+const leftbtn=document.querySelector(".hide-aside-button");
+const asider=document.querySelector(".aside");
+
+rightbtn.addEventListener("click",()=>{
+    rightbtn.style.display='none';
+    asider.style.display='block';
+    leftbtn.style.display='block';
+})
+leftbtn.addEventListener("click",()=>{
+    leftbtn.style.display='none';
+    asider.style.display='none';
+    rightbtn.style.display='block';
+})
+
+const activeli=document.querySelectorAll(".aside ul li");
+const updateactive=()=>{
+    activeli.forEach(item=>{
+        item.classList.remove("active");
+    })
+}
+
+activeli.forEach(ele => {
+    ele.addEventListener("click",()=>{
+        updateactive();
+        ele.classList.add("active");
+    })
+});
